a657b6cd638d367a6e4e27d12a7a45b9f036a9fe
jeaninerees <jeanine.rees@gmail.com>
2016-04-08 13:57:01 -0400

    Rees homework 4

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
 
+$(".readlessHide").hide();
 $(".readmore").click(readMore);
-$(".readlessHide").hide;
+$(".readlessHide").click(readLess);
+$(".learnmore").click(learnMore);
+$(".button blue").click(event);
 
 function readMore() {
 	$("#show-this-on-click").slideDown();
+	$(".readmore").hide();
 	$(".readlessHide").show();
 }
 
+function readLess() {
+	$(".readlessHide").hide();
+	$(".hide").slideUp();
+	$(".readmore").show();
+}
 
+function learnMore() {
+	$("#learnmoretext").slideDown();
+	$(".learnmore").hide();
+}
 
+function event() {
+	(".button blue").preventDefault;
+}
 
 });
